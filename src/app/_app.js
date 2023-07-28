42a56109ef1e69f52f9fb1a5d97e7bbaf33f8c61
Leonid <asal9000@gmail.com>
2023-07-29 01:26:36 +0300

    fix

--- src/app/_app.js
+++ src/app/_app.js
@@ -3,9 +3,15 @@ import Script from "next/script";
 export default function App({ Component, pageProps }) {
   return;
   <>
-    <Script data-b24-form="click/20/5aff5t" data-skip-moving="true">
-      {`(function(w,d,u){var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/180000|0);var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);})(window,document,'https://cdn-ru.bitrix24.ru/b25437620/crm/form/loader_20.js');`}
-    </Script>
+    <Script
+      id="birtix24"
+      data-b24-form="click/20/5aff5t"
+      data-skip-moving="true"
+      strategy="beforeInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `(function(w,d,u){var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/180000|0);var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);})(window,document,'https://cdn-ru.bitrix24.ru/b25437620/crm/form/loader_20.js');`,
+      }}
+    />
     <Component {...pageProps} />;
   </>;
 }
